4bf143ad5ea49b9e4099616a05a69ce1aa75c8ca
Renan <renan.lima4946381@edu.pe.senac.br>
2025-07-30 08:49:07 -0300

    Esta é a junção da segunda e terceira etapas do projeto, onde criamos duas novas classes, a classe Cliente e a classe Aluno, nelas criamos variáveis, atributos e fornecemos valores aos mesmos, nela também realizamos um processo de herança, herança essa feita a partir da primeira classe (Veículo). Por fim utilizamos o Override, que é um conceito que permite que uma subclasse forneça uma implementação específica de um método já definido em sua superclasse.

---
+++ Projeto.java
@@ -0,0 +1,39 @@
+package TransTakwary;
+
+public class Main {
+	public static void main(String[] args) {
+		Veículo v1 = new Veículo("Avenida Agamenon Magalhães - Rua Coronel Tejo", 123456);
+		Cliente cl1 = new Cliente("Eduardo", 4908132);
+		
+		System.out.println("O nome do cliente é " + cl1.getNome());
+		
+		cl1.setCadastro(3568369);
+		System.out.println("--Fazendo Cadastro--");
+		System.out.println("Emitindo código...");
+		System.out.println("O código cadastral do cliente " + cl1.getNome() + " é: " + cl1.getCadastro() + " .");
+		
+		v1.adicionar_veiculo();
+		cl1.ingressar();
+		
+		v1.setID(9123445);
+		System.out.println("A identificação do veículo de rota: " + v1.getrota() + " é: " + v1.getID() + " .");
+		
+		}
+	
+}
+
+import TransTakwary.Cliente;
+public class Cliente extends Veículo{
+	int cadastro;
+	String nome;
+	
+	public Aluno(String nome, int cadastro, String rota) {
+		super(rota);
+		this.cadastro = cadastro;
+	}
+	
+	@Override
+	void ingressar() {
+		System.out.print("O cliente está efetivando o ingresso de reserva no serviço veicular.");
+	}
+}
