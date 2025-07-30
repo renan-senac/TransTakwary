b4a32a29cad30c31094ecfde7b037bf1466ff8c8
Renan <renan.lima4946381@edu.pe.senac.br>
2025-07-30 08:08:17 -0300

    Essa é a classe main do código, onde fazemos uso das duas classes protagonistas, que possuem atributos e por meio deles realizam ações. Mais especificamente é nesta etapa do processo em que temos a emissão do código cadastral de nossos clientes e a identificação do ID veicular a partir da respectiva rota. Aqui, fazemos uso dos métodos Get e Set que possuem a finalidade de acessar e modificar atributos (variáveis) de uma classe.

--- Projeto.java
+++ Projeto.java
@@ -1,2 +1,26 @@
-//Iniciando Projeto de serviço de tranporte 
+package TransTakwary;
+
+public class Main {
+	public static void main(String[] args) {
+		Veículo v1 = new Veículo("Avenida Agamenon Magalhães - Rua Coronel Tejo", 123456);
+		Cliente cl1 = new Cliente("Eduardo", 4908132);
+		
+		System.out.println("O nome do cliente é " + cl1.getNome());
+		
+		cl1.setCadastro(3568369);
+		System.out.println("--Fazendo Cadastro--");
+		System.out.println("Emitindo código...")
+		System.out.println("O código cadastral do cliente " + cl1.getNome() + " é: " + cl1.getCadastro() + " .");
+		
+		v1.adicionar_veiculo();
+		cl1.ingressar();
+		
+		v1.setID(9123445);
+		System.out.println("A identificação do veículo de rota: " + v1.getRota() + " é: " + v1.getID() + " .");
+		
+		
+	}
+	
+
+}
 
